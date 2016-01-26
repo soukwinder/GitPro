01e0493be3ed662a1ac11e87d99888a1cc248c9c
soukwinder <sanjosinghmaddan@hotmail.com>
2016-01-26 13:04:30 +0100

    ho
    ihb

---
+++ src/main/java/com/souk/GitPro/App.java
@@ -0,0 +1,13 @@
+package com.souk.GitPro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
